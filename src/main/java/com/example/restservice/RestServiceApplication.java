072b9c2694318ab9f95f7a6f012d72e141794ecb
marcoscga <86233895+marcoscga@users.noreply.github.com>
2021-08-31 12:50:55 +0200

    Commit 1 marcoscga

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestServiceApplication {
 
+	//TODO @marcoscga comentario jodón
     public static void main(String[] args) {
+    	System.out.println("Print de consola");
         SpringApplication.run(RestServiceApplication.class, args);
     }
-
+    
+    public static void blabla() {
+    	System.out.println("Print de consola");
+    }
 }
